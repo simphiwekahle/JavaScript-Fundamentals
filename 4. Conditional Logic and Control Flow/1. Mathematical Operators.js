819eaf9c0017a2b16309678f1cbda6d822634b05
Sphephelo <sphephelon@hollywoodbets.net>
2025-01-28 10:11:05 +0200

    Conditional Logic and Control Flow Mathematical operators

---
+++ 4. Conditional Logic and Control Flow/1. Mathematical Operators.js
@@ -0,0 +1,44 @@
+// Addition +
+let val1 = 3 + 2;
+console.log(`Val1: ${val1}`);
+let val2 = 2 + -1;
+console.log(`Val2: ${val2}`);
+
+// Subtraction -
+let val3 = 3 - 1;
+console.log(`Val3: ${val3}`);
+let val4 = 1 - -1;
+console.log(`Val4: ${val4}`);
+
+// Diviion /
+let val5 = 3 / 1;
+console.log(`Val5: ${val5}`);
+let val6 = 21 / 9;
+console.log(`Val6: ${val6.toFixed(2)}`);
+
+// Multiplication *
+let val7 = 3 * 2;
+console.log(`Val7: ${val7}`);
+let val8 = 3 * 3;
+console.log(`Val8: ${val8}`);
+
+// Remainder %
+let val9 = 9 % 2;
+console.log(`Val9: ${val9}`);
+let val10 = -10 % 7;
+console.log(`Val10: ${val10}`);
+
+// Exponentiation **
+let val11 = 3 ** 2;
+console.log(`Val11: ${val11}`);
+let val12 = 3 ** -1;
+console.log(`Val12: ${val12}`);
+
+// Increment (++) and Decrement (--)
+let val13 = 1;
+++val13; // val13 = val13 + 1
+console.log(`Val13: ${val13}`);
+
+let val14 = 1;
+--val14; // val14 = val14 - 1
+console.log(`Val14: ${val14}`);
